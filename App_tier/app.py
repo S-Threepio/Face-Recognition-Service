fb05c5c61a3dc5c0806b5a4789b5fbe51c518d03
cult_mentality <kavitkarswanand5@gmail.com>
2023-02-17 21:35:49 -0700

    added code for s3, image classification with queue

--- App_tier/app.py
+++ App_tier/app.py
@@ -1,4 +1,3 @@
-import boto3
 import torch
 import torchvision
 import torchvision.transforms as transforms
@@ -11,45 +10,57 @@ import numpy as np
 import json
 import sys
 import time
+import boto3
 
 
-sqs = boto3.resource('sqs', region_name='us-east-1')
-
-# for queue in sqs.queues.all():
-#     print(queue.url)
-
-
-request_fifo = sqs.Queue(sqs.get_queue_by_name(QueueName='Request.fifo').url)
-response_fifo = sqs.Queue(sqs.get_queue_by_name(QueueName='Response.fifo').url)
-
-while(1) :
-        messages = request_fifo.receive_messages(MessageAttributeNames=['All'],MaxNumberOfMessages=10,WaitTimeSeconds=10)
-        if not len(messages):
-                break
-        for msg in messages:
-                print("Received message: %s: %s", msg.message_id, msg.body)
-                message = predict(url)
-                response = response_fifo.send_message(MessageBody=message,MessageGroupId='imageData')
-                print(response.get('MessageId'))                
-                msg.delete()
+def write_to_S3(body, key):
+    s3 = boto3.client("s3")
+    s3.put_object(Body=body, Bucket="app-tier", Key=key + ".txt")
 
 
-def predict(url) :
-        url = str(sys.argv[1])
-        #img = Image.open(urlopen(url))
-        img = Image.open(url)
+def classify_image(url):
+    # url = str(sys.argv[1])
+    img = Image.open(urlopen(url))
+    # img = Image.open(url)
 
-        model = models.resnet18(pretrained=True)
+    model = models.resnet18(pretrained=True)
 
-        model.eval()
-        img_tensor = transforms.ToTensor()(img).unsqueeze_(0)
-        outputs = model(img_tensor)
-        _, predicted = torch.max(outputs.data, 1)
+    model.eval()
+    img_tensor = transforms.ToTensor()(img).unsqueeze_(0)
+    outputs = model(img_tensor)
+    _, predicted = torch.max(outputs.data, 1)
 
-        with open('./imagenet-labels.json') as f:
+    with open("./imagenet-labels.json") as f:
         labels = json.load(f)
-        result = labels[np.array(predicted)[0]]
-        img_name = url.split("/")[-1]
-        #save_name = f"({img_name}, {result})"
-        save_name = f"{img_name},{result}"
-        print(f"{save_name}")
+    result = labels[np.array(predicted)[0]]
+    img = url.split("/")[-1]
+    # save_name = f"({img_name}, {result})"
+    img_name = img.split(".")[0]
+    save_name = f"({img_name},{result})"
+    print(f"{save_name}")
+    write_to_S3(save_name, img_name)
+    output = {"name": img, "output": result}
+    return json.dumps(output)
+
+
+sqs = boto3.resource("sqs", region_name="us-east-1")
+
+request_fifo = sqs.Queue(sqs.get_queue_by_name(QueueName="Request.fifo").url)
+response_fifo = sqs.Queue(sqs.get_queue_by_name(QueueName="Response.fifo").url)
+
+while 1:
+    messages = request_fifo.receive_messages(
+        MessageAttributeNames=["All"], MaxNumberOfMessages=10, WaitTimeSeconds=10
+    )
+    if not len(messages):
+        break
+    for msg in messages:
+        print("Received url: ", msg.body)
+        # body = json.loads(msg.body)
+        result = classify_image(msg.body)
+        response = response_fifo.send_message(
+            MessageBody=result, MessageGroupId="imageData"
+        )
+        print(response.get("MessageId"))
+        print(result)
+        msg.delete()
